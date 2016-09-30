b1372c880a19195f5bab749fb2e843c70ddd1c13
Teguayco <alu0100825503@ull.edu.es>
2016-09-30 10:39:37 +0100

    subiendo proyecto

---
+++ Assets/BallBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponent<Renderer> ().material.color = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown() {
+		this.gameObject.GetComponent<Rigidbody>().AddForce (transform.forward * 1000);
+	}
+}
